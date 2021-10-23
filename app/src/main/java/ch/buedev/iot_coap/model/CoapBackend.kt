55d734b5fb594eda0beaa3f93a60a5d92b7975fc
Cédric Bühler <cedric.buehler@raiffeisen.ch>
2021-10-23 17:40:16 +0200

    Import new parcelize package

--- app/src/main/java/ch/buedev/iot_coap/model/CoapBackend.kt
+++ app/src/main/java/ch/buedev/iot_coap/model/CoapBackend.kt
@@ -1,8 +1,7 @@
 package ch.buedev.iot_coap.model
 
 import android.os.Parcelable
-import kotlinx.android.parcel.Parcelize
-
+import kotlinx.parcelize.Parcelize
 
 @Parcelize
 data class CoapBackend(
